9d3e64031f53287a120563e561a47dafcf66fd21
kyawthetkhaing255 <kyawthetkhaing255@gmail.com>
2025-03-27 15:33:58 +0630

    changed into PWA

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "pwa-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/192.png",
+  "/512.png",
+];
+
+// Install Service Worker and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching files...");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Fetch from cache when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
